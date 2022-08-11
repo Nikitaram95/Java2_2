9bcca9792652807189f11e8efa66be702fee59a1
Nikitaram95 <105075205+Nikitaram95@users.noreply.github.com>
2022-08-11 15:35:12 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.net.SocketOption;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int refill = 1000;
+        int balance = 100;
+
+        int percent = 1;
+        if (refill < 1000) {
+            percent = 0;
+        }
+        int bonus = refill * percent / 100;
+        int sum = bonus + balance + refill;
+        System.out.println("Итоговая сумма: " + sum);
+
+    }
+}
